cdhowie/queson

commit e2a559e4e8b0d4b7c43c692d2c3746153810cdd7

Allow parse_any to be inlined

src/de.rs
@@ -35,6 +35,8 @@ enum Thunk<D: Deserialization> {
         /// The next key to be added.
         key: D::String,
     },
+
+    Done,
 }
 
 /// A JSON parse error.
@@ -875,7 +877,7 @@ fn parse_json_with<D: Deserialization>(
 
     let mut stack = vec![];
 
-    let mut last_any = parse_any(deserialization, &mut json);
+    let mut last_any = ThunkResult::Thunk(Thunk::Done);
 
     let result = loop {
         last_any = match last_any {
@@ -893,18 +895,16 @@ fn parse_json_with<D: Deserialization>(
                 parse_any(deserialization, &mut json)
             }
 
-            ThunkResult::Ok(value) => match stack.pop() {
-                Some(op) => match op {
-                    Thunk::ParsingList(list) => {
-                        continue_parse_list(deserialization, list, value, &mut json)
-                    }
+            ThunkResult::Ok(value) => match stack.pop().unwrap() {
+                Thunk::ParsingList(list) => {
+                    continue_parse_list(deserialization, list, value, &mut json)
+                }
 
-                    Thunk::ParsingMap { dict, key } => {
-                        continue_parse_map(deserialization, dict, key, value, &mut json)
-                    }
-                },
+                Thunk::ParsingMap { dict, key } => {
+                    continue_parse_map(deserialization, dict, key, value, &mut json)
+                }
 
-                None => break value,
+                Thunk::Done => break value,
             },
         };
     };

commit fa8b83c413c913d76d1165dc51e9533e58187a99

Fix depth_limit off-by-one regression introduced in e2a559e4e8b0d4b7c43c692d2c3746153810cdd7

src/de.rs
@@ -872,7 +872,7 @@ fn parse_json_with<D: Deserialization>(
             ThunkResult::Thunk(op) => {
                 stack.push(op);
 
-                if depth_limit.is_some_and(|limit| stack.len() >= limit.into()) {
+                if depth_limit.is_some_and(|limit| stack.len() > limit.into()) {
                     return Err((ParseError::DepthLimitExceeded, len - json.len()));
                 }
 

tests/test_queson.py
@@ -214,3 +214,11 @@ def test_circular() -> None:
 
     with pytest.raises(ValueError):
         queson.dumpb(cycle, check_circular=True)
+
+def test_specific_depth_limit() -> None:
+    source = b'[{"...":"...","arg":[[[[[[[[[[[[[1]]]]]]]]]]]]]}]'
+
+    with pytest.raises(ValueError):
+        queson.loadb(source, depth_limit=15)
+
+    queson.loadb(source, depth_limit=16)